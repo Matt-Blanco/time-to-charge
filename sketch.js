335634a1c39d97c255a45c6f942d42004c2104c5
Matt-Blanco <mattmblanco@gmail.com>
2026-04-16 17:09:38 -0400

    Updates

--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+const batteryLimit = 3700; // Samsung A7 Lite Batter capacity is 3700mAh
+const showBatteryInfo = false;
 let battery = {
   level: 0.0,
   charging: false,
@@ -5,20 +7,43 @@ let battery = {
   dischargingTime: 0,
 };
 
-let hapticOn = 0;
+let chargingCount = 0;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(220);
 
-  text(`charging: ${battery.charging}`, width / 2, height / 2);
-  text(`charge time ${battery.chargingTime / 3600} hour(s)`, width / 2, height / 2 - 20);
-  text(`discharge time: ${battery.dischargingTime}`, width / 2, height / 2 - 30);
-  text(`battery level: ${battery.level}`, width / 2, height / 2 - 50);
-  text(`haptic cound: ${hapticOn}`, width / 2, height / 2 - 60);
+  if (frameCount % 15 === 0) {
+    if (battery.charging) {
+      chargingCount++;
+    } else {
+      chargingCount--;
+    }
+  }
+
+  textFont("lincoln-electric-regular");
+  fill(0, lerp(0, 100, map(chargingCount, 0, 50, 0, 1)));
+  textSize(50);
+  text("What is a Joule?", width / 2 - 50, 50);
+  text(`charge count: ${chargingCount}`, width / 2 - 50, 150);
+
+  if (showBatteryInfo) {
+    text(`charging: ${battery.charging}`, width / 2, height / 2);
+    text(
+      `charge time ${battery.chargingTime / 3600} hour(s)`,
+      width / 2,
+      height / 2 - 20,
+    );
+    text(
+      `discharge time: ${battery.dischargingTime}`,
+      width / 2,
+      height / 2 - 30,
+    );
+    text(`battery level: ${battery.level}`, width / 2, height / 2 - 50);
+  }
 }
 
 navigator.getBattery().then((bat) => {
@@ -40,11 +65,3 @@ navigator.getBattery().then((bat) => {
     battery = bat;
   });
 });
-
-function triggerHaptic() {
-  console.log('triggering haptic')
-  navigator.vibrate(500);
-  hapticOn++;
-}
-
-setInterval(() => triggerHaptic(), 1000)
